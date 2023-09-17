8b79d21a98d0fefad6a4678b2b20636e1bee2a23
+ <+>
2023-09-17 16:55:28 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 console.log('%c HI', 'color: firebrick')
+
+const httpUrl = "https://dog.ceo/api/breeds/image/random/4";
+const dogDropDown = document.querySelector("#breed-dropdown")
+const firstDogBreed = document.querySelector("#dog-image-container")
+
+
+//dog-image-container.appendChild(imgUrl)
+function setDogImage(imgUrl){
+    const imgOfDog = document.createElement("img");
+
+    firstDogBreed.innerHTML = "";
+    imgOfDog.style.width = "200px";
+    imgOfDog.setAttribute("src", imgUrl);
+    firstDogBreed.appendChild(imgOfDog);
+}
+
+async function getDogImages() {
+    const response = await fetch(httpUrl);
+    const data = await response.json();
+    return data.message;
+}
+
+getDogImages().then(function(dogImages) {
+    setDogImage(dogImages[0])
+
+    dogDropDown.addEventListener("change", function(e){  
+        const index = parseInt(e.target.value);
+        const dogImage = dogImages[index];
+        setDogImage(dogImage)
+    })   
+});
